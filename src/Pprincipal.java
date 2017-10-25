e8a9a89d973497b506278467d27cb16f5c3069ca
javiergutierrezp <javiergutzp@hotmail.com>
2017-10-25 09:49:01 -0500

    Agregando buscar paciente

--- src/Pprincipal.java
+++ src/Pprincipal.java
@@ -28,25 +28,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import javax.swing.table.*;
+import java.sql.*;
+import java.util.*;
 
 public class Pprincipal extends JFrame{
-	
+
 	private JButton buttonSubmit,Pacientes, RegistrarPaciente, CapturaReceta, NuevoEvento, Emergencia, ReOrdenMed, PacientesPorMedicar;
 	private JTextField BuscarPaciente;
 
 	public Pprincipal(){
-	createUI();
+		createUI();
 
-	//Make window exit appication on close
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
-	Toolkit tk = Toolkit.getDefaultToolkit();
-	int xSize = ((int) tk.getScreenSize().getWidth());
-	int ySize = ((int) tk.getScreenSize().getHeight());
-	setSize(xSize,ySize);
-	setLocationRelativeTo(null);
-	//Disable resize
-	setResizable(false);
-	} 
+		//Make window exit appication on close
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		int xSize = ((int) tk.getScreenSize().getWidth());
+		int ySize = ((int) tk.getScreenSize().getHeight());
+		setSize(xSize,ySize);
+		setLocationRelativeTo(null);
+		//Disable resize
+		setResizable(false);
+	}
 
 	//Initializes all UI Components
 	private void createUI(){
@@ -65,29 +68,11 @@ public class Pprincipal extends JFrame{
 		buttonSubmit.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent event){
-				String nombrePx = BuscarPaciente.getText();
-				try {
-					Class.forName("com.mysql.jdbc.Driver");
-
-					//Get connection with MySQL database
-					Connection mycon = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistema_asilo?useSSL=false",
-							"root", "1212");
-					Statement myStmt = mycon.createStatement();
-					//Adds the new score to the database
-					String mySQLTable = "paciente";
-
-					String query = "select * from paciente where nombre like '" + nombrePx + "%' or apellido like '" + nombrePx +"%'";;
-
-					//Execute SQL query
-					ResultSet myRs = myStmt.executeQuery(query);
-
-					while (myRs.next()) {
-						System.out.println(myRs.getString(4)); //gets the first column's rows.
-					}
-				} catch (Exception e) {
-					System.out.println("Error with database connection");
-					e.printStackTrace();
-				}
+				JFrame frame = new JFrame();
+				frame.add(TableFromSql());
+				frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+				frame.pack();
+				frame.setVisible(true);
 			}
 		});
 		panelNorth.add(buttonSubmit, BorderLayout.EAST);
@@ -104,7 +89,6 @@ public class Pprincipal extends JFrame{
 		Pacientes.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e){
-
 			}
 		});
 
@@ -171,12 +155,12 @@ public class Pprincipal extends JFrame{
 		});
 		panelWest.add(PacientesPorMedicar, d);
 
-		
+
 		//CENTER
-		
+
 		JPanel panelCenter = new JPanel();
 		panelCenter.setLayout(new GridBagLayout());
-		JTabbedPane jTP = new JTabbedPane();		
+		JTabbedPane jTP = new JTabbedPane();
 		GridBagConstraints myConstraints = new GridBagConstraints();
 		myConstraints.weightx = myConstraints.weighty = 1.0;
 		myConstraints.gridx=0;
@@ -189,22 +173,117 @@ public class Pprincipal extends JFrame{
 	}
 	//Lo que va dentro de pacientes por medicar
 	protected JComponent PxPorMedicar(String text) {
-    	JPanel panelG = new JPanel(false);
-    	JLabel filler = new JLabel(text);
-    	filler.setHorizontalAlignment(JLabel.CENTER);
-    	panelG.setLayout(new GridLayout(1, 1));
-    	panelG.add(filler);
-    	return panelG;
+		JPanel panelG = new JPanel(false);
+		JLabel filler = new JLabel(text);
+		filler.setHorizontalAlignment(JLabel.CENTER);
+		panelG.setLayout(new GridLayout(1, 1));
+		panelG.add(filler);
+		return panelG;
 	}
 
 	//Lo que va dentro de Reorden Meds
 	protected JComponent ReOrMeds(String text) {
-    	JPanel panelG = new JPanel(false);
-    	JLabel filler = new JLabel(text);
-    	filler.setHorizontalAlignment(JLabel.CENTER);
-    	panelG.setLayout(new GridLayout(1, 1));
-    	panelG.add(filler);
-    	return panelG;
+		JPanel panelG = new JPanel(false);
+		JLabel filler = new JLabel(text);
+		filler.setHorizontalAlignment(JLabel.CENTER);
+		panelG.setLayout(new GridLayout(1, 1));
+		panelG.add(filler);
+		return panelG;
+	}
+
+	private JPanel TableFromSql (){
+		String nombrePx = BuscarPaciente.getText();
+
+		ArrayList columnNames = new ArrayList();
+		ArrayList data = new ArrayList();
+
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//Get connection with MySQL database
+			Connection mycon = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistema_asilo?useSSL=false",
+					"root", "1212");
+			Statement myStmt = mycon.createStatement();
+			//Adds the new score to the database
+			String mySQLTable = "paciente";
+
+			String query = "select * from paciente where nombre like '" + nombrePx + "%' or apellido like '" + nombrePx +"%'";;
+
+			//Execute SQL query
+			ResultSet myRs = myStmt.executeQuery(query);
+
+			ResultSetMetaData md = myRs.getMetaData();
+			int columns = md.getColumnCount();
+
+			//Get column names
+			for (int i = 1; i<= columns; i++){
+				columnNames.add(md.getColumnName(i));
+			}
+			//Get row data
+			while (myRs.next()){
+				ArrayList row = new ArrayList(columns);
+				for (int i = 1; i<= columns; i++){
+					row.add(myRs.getObject(i));
+				}
+				data.add(row);
+			}
+
+			/*while (myRs.next()) {
+			System.out.println(myRs.getString(4)); //gets the first column's rows.
+			}*/
+		}
+		catch (Exception e) {
+			System.out.println("Error with database connection");
+			e.printStackTrace();
+		}
+		// Create Vectors and copy over elements from ArrayLists to them
+		// Vector is deprecated but I am using them in this example to keep
+		// things simple - the best practice would be to create a custom defined
+		// class which inherits from the AbstractTableModel class
+		Vector columnNamesVector = new Vector();
+		Vector dataVector = new Vector();
+
+		for (int i = 0; i<data.size(); i++){
+			ArrayList subArray = (ArrayList)data.get(i);
+			Vector subVector = new Vector();
+			for (int j = 0; j < subArray.size(); j++){
+				subVector.add(subArray.get(j));
+			}
+			dataVector.add(subVector);
+		}
+
+		for (int i = 0; i < columnNames.size(); i++ )
+			columnNamesVector.add(columnNames.get(i));
+
+		//  Create table with database data
+		JTable table = new JTable(dataVector, columnNamesVector)
+		{
+			public Class getColumnClass(int column)
+			{
+				for (int row = 0; row < getRowCount(); row++)
+				{
+					Object o = getValueAt(row, column);
+
+					if (o != null)
+					{
+						return o.getClass();
+					}
+				}
+
+				return Object.class;
+			}
+		};
+
+		JPanel sendPanel = new JPanel(new BorderLayout());
+		JScrollPane scrollTable = new JScrollPane(table);
+		sendPanel.add(scrollTable);
+
+		JPanel buttonPanel = new JPanel();
+		sendPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+		return sendPanel;
 	}
 
 	//Entry point of the program
@@ -220,5 +299,5 @@ public class Pprincipal extends JFrame{
 
 	}
 
-	
+
 }
