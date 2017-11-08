b49cb1fe0188693e3766a90cdb82d7b9a785fdcc
cogp91 <omargalvezp8@gmail.com>
2017-11-08 09:49:48 -0600

    agregar Pdespliega y modificar su controlador

---
+++ src/sample/clases/Pdespliega.java
@@ -0,0 +1,22 @@
+package sample.clases;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Pdespliega extends Application{
+
+    @Override
+    public void start (Stage thirdStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/sample/pantallas/Pdespliega.fxml"));
+        Parent root3 = loader.load();
+        thirdStage.setTitle("Perfil de Paciente");
+        thirdStage.setScene(new Scene(root3, 900, 700));
+        thirdStage.show();
+    }
+
+    public static void main(String[] args){ launch(args);}
+}
