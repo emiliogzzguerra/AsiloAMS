16a75b6306c80d19e1dea06bb3ab67c3b2ee1290
cogp91 <omargalvezp8@gmail.com>
2017-11-08 11:31:02 -0600

    avance de la busqueda avanzada

--- src/sample/controllers/Controller.java
+++ src/sample/controllers/Controller.java
@@ -2,7 +2,8 @@ package sample.controllers;
 
 import javafx.scene.control.*;
 import javafx.stage.Stage;
-//import sample.clases.BuscarId;
+import sample.clases.BuscarId;
+import org.controlsfx.control.textfield.TextFields;
 import sample.clases.Pdespliega;
 import sample.clases.RegistroPx;
 
@@ -14,16 +15,8 @@ public class Controller {
 
 
     public void advancedSearch(){
-        //String[] possWords = {"Jai","Emilio","Carlos","Javier","te la comes"};
-        //TextFields.bindAutoCompletion(textBuscar, possWords);
-        //String nameSelected = textBuscar.getText();
-        //Metodo para buscar el id con el nombre y apellido del paciente usando el nuevo string en textBuscar
-        //BuscarId buscarId = new BuscarId();
-        //int ID = buscarId.buscarPxId(nameSelected);
-        //Metodo para abrir nueva ventana del Px dando el id del paciente
-        //PerfilPaciente nuevoPerfil = PerfilPaciente();
-        //nuevoPerfil.abrir(ID);
-
+        String[] possWords = {"Jai","Emilio","Carlos","Javier","te la comes"};
+        TextFields.bindAutoCompletion(textBuscar, possWords);
     }
 
     public void buscarPx () {
