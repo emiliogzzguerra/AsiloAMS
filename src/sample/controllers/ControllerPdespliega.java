1cb3616adf9058b18c3661f4fab1532d7f16abba
Alejandro Gomez <mrcimi1@hotmail.com>
2017-11-14 16:45:39 -0600

    quitar imports

--- src/sample/controllers/ControllerPdespliega.java
+++ src/sample/controllers/ControllerPdespliega.java
@@ -1,15 +1,9 @@
 package sample.controllers;
 
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.stage.Stage;
-import sample.clases.Pdespliega;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import org.controlsfx.control.textfield.TextFields;
-import javafx.scene.control.Label;
-import javafx.event.EventHandler;
-import javafx.scene.control.TextArea;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
