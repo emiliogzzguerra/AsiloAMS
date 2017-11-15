0329ccac48262fa9b19830b0414559f589790e15
cogp91 <omargalvezp8@gmail.com>
2017-11-14 18:19:45 -0600

    avance de modelReceta, agregar medicamento_manana, medicamento_tarde al constructor de paciente

---
+++ src/sample/modelos/ModelReceta.java
@@ -0,0 +1,60 @@
+package sample.modelos;
+
+import sample.objetos.Receta;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ModelReceta {
+
+    public boolean insertar(Receta receta) {
+
+        Statement myStmt = GeneralModel.connect();
+
+        String query = "INSERT INTO ";
+        query += "receta ";
+        String sql = new StringBuilder()
+                .append("(identificador, fecha_expedicion) VALUES (")
+                .append(receta.getIdentificador()) // identificador
+                .append(",'")
+                .append(receta.getFecha_expedicion()) // fecha_expedicion
+                .append(")")
+                .toString();
+        try{
+            myStmt.executeUpdate(query);
+            return true;
+        } catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    public Receta getReceta (Integer id) {
+
+
+        String query = "select * from receta where id = " + id.toString();
+        ArrayList columnNames = new ArrayList();
+
+        Statement myStmt = GeneralModel.connect();
+        try {
+            ResultSet myRs = myStmt.executeQuery(query);
+            ResultSetMetaData md = myRs.getMetaData();
+            int columns = md.getColumnCount();
+
+            //Get column names
+            for (int i = 1; i<= columns; i++){
+                columnNames.add(md.getColumnName(i));
+            }
+
+            //Insertar informacion a objeto deseado
+            Receta recetaAuxiliar = new Receta(myRs.getString(1), myRs.getDate(2));
+
+            //Retornar objeto
+            return recetaAuxiliar;
+        } catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+}
