8da416df372acb91658e1aab50ffc81e0bca19e6
Alejandro Gomez <mrcimi1@hotmail.com>
2017-11-15 05:14:09 -0600

    fef

--- src/sample/controllers/ControllerPdespliega.java
+++ src/sample/controllers/ControllerPdespliega.java
@@ -2,10 +2,13 @@ package sample.controllers;
 
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import java.net.URL;
+import java.nio.Buffer;
 import java.util.ResourceBundle;
 
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import sample.clases.setImage;
@@ -16,23 +19,13 @@ import sample.objetos.Paciente;
 
 public class ControllerPdespliega implements Initializable {
     @FXML
-    public Label desNombre;
-    @FXML
-    public Label desDireccion;
-    @FXML
-    public Label desEdad;
-    @FXML
-    public Label desCuarto;
-    @FXML
-    public Label desCama;
-    @FXML
-    public Label desEstatus;
-    @FXML
-    public Label desDescEvento;
-
+    public Label desNombre, desDireccion, desEdad, desCuarto, desCama, desEstatus, desDescEvento;
     @FXML
     public ImageView fotoPx;
-
+    @FXML
+    public TextField desEdNombre, desEdDireccion, desEdEdad, desEdCuarto, desEdCama, desEdEstatus;
+    @FXML
+    public Button editarCampos, guardarCampos;
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
         Controller iD = new Controller();
@@ -58,8 +51,8 @@ public class ControllerPdespliega implements Initializable {
         desNombre.setText(nombreCompleto);
         desDireccion.setText(direccionCompleta);
         desEdad.setText(String.valueOf(p.getEdad()));
-        desCuarto.setText(String.valueOf(p.getNumero_cuarto()));
-        desCama.setText(String.valueOf((p.getNumero_cama())));
+        desCuarto.setText(String.valueOf(p.getNumero_cuarto().get()));
+        desCama.setText(String.valueOf((p.getNumero_cama()).get()));
         desEstatus.setText(String.valueOf(p.getEstatus()));
 
 
@@ -71,5 +64,61 @@ public class ControllerPdespliega implements Initializable {
 
 
 
+    }
+    @FXML
+    public void crearEditar() {
+        editarCampos.setVisible(false);
+
+        desNombre.setVisible(false);
+        desDireccion.setVisible(false);
+        desCuarto.setVisible(false);
+        desCama.setVisible(false);
+        desEstatus.setVisible(false);
+
+        guardarCampos.setVisible(true);
+
+        desEdNombre.setVisible(true);
+        desEdNombre.setText(desNombre.getText());
+
+        desEdDireccion.setVisible(true);
+        desEdDireccion.setText(desDireccion.getText());
+
+        desEdCuarto.setVisible(true);
+        desEdCuarto.setText(desCuarto.getText());
+
+        desEdCama.setVisible(true);
+        desEdCama.setText(desCama.getText());
+
+        desEdEstatus.setVisible(true);
+        desEdEstatus.setText(desEstatus.getText());
+
+    }
+
+    @FXML
+    public void guardarCampos(){
+        editarCampos.setVisible(true);
+
+        desEdNombre.setVisible(false);
+        desEdDireccion.setVisible(false);
+        desEdCuarto.setVisible(false);
+        desEdCama.setVisible(false);
+        desEdEstatus.setVisible(false);
+
+        desNombre.setVisible(true);
+        desDireccion.setVisible(true);
+        desCuarto.setVisible(true);
+        desCama.setVisible(true);
+        desEstatus.setVisible(true);
+
+        desNombre.setText(desEdNombre.getText());
+        desDireccion.setText(desEdDireccion.getText());
+        desEdad.setText(desEdad.getText());
+        desCuarto.setText(desEdCuarto.getText());
+        desCama.setText(desEdCama.getText());
+        desEstatus.setText(desEdEstatus.getText());
+
+
+
+        guardarCampos.setVisible(false);
     }
 }
