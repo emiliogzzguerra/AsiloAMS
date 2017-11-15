f0f432c37e20ef7f52dda89ca756f651507b304c
cogp91 <omargalvezp8@gmail.com>
2017-11-15 06:09:57 -0600

    te la comes

--- src/sample/controllers/ControllerReOrden.java
+++ src/sample/controllers/ControllerReOrden.java
@@ -4,6 +4,11 @@ import sample.modelos.ModelMedicamento;
 import sample.objetos.Medicamento;
 
 public class ControllerReOrden {
+
+    public void Prueba (){
+        obtenerMedicamentos(7);
+    }
+
     public Medicamento[] obtenerMedicamentos(Integer dias){
         ModelMedicamento mm = new ModelMedicamento();
         return mm.getMedicamentosReorden(2, dias);
